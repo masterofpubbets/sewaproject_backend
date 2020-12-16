8f1c7dc3a966787b2c8a5d2ba73142412f36c6f2
masterofpubbets <master.pubbets@gmail.com>
2020-12-16 12:36:08 +0400

    Remove Test From Package.json

--- routers/instRouter.js
+++ routers/instRouter.js
@@ -34,4 +34,82 @@ router.post('/instrumenttop', auth, async (req, res) => {
     }
 });
 
+router.get('/summarymini', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_insSummaryMini')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.get('/summary', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_InsSummary')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.get('/calibrationsummary', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_CalibrationSummary')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.get('/hookupsummarymini', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_instHookupSummaryMini')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.get('/hookupsummary', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_InsHkSummary')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.get('/list', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .execute('sp_getInstListMini')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
+router.post('/detail', auth, async (req, res) => {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+        .input('tag', sql.NVarChar(100), req.body.tag)
+        .execute('sp_getInstDetail')
+        return res.status(200).send(result.recordsets)
+    } catch (er) { 
+        return res.status(400).send(er);
+    }
+});
+
 module.exports = router;
