cd0de51290bd187672467d2cef5ce7ab61b36aec
masterofpubbets <master.pubbets@gmail.com>
2021-03-03 09:58:18 +0400

    Modify user type min length to 2 chars

--- users/userSchema.js
+++ users/userSchema.js
@@ -14,7 +14,7 @@ const userSchema = new mongo.Schema( {
 const userValidate = Joi.object({
     userName: Joi.string().min(4).max(50).required(),
     fullName: Joi.string().min(4).max(50).required(),
-    userType: Joi.string().min(4).max(20).required(),
+    userType: Joi.string().min(2).max(20).required(),
     password: Joi.string().min(5).max(20).required()
 });
 
